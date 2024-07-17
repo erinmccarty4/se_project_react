52c5e78a59f1538654ff15716d0eb6639da43a5d
Your Name <you@example.com>
2024-07-17 18:36:53 -0400

    main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/App/App.jsx";
-import "./index.css";
+import "./main.css";
+import WeatherCard from "../WeatherCard/WeatherCard";
+// import { defaultClothingItems } from "../../utils/constants";
+import ItemCard from "../ItemCard/ItemCard";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
+import { useContext } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+function Main({ weatherData, onCardClick, clothingItems }) {
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+  const temp = weatherData?.temp?.[currentTemperatureUnit] || 999;
+
+  return (
+    <main className="main">
+      {" "}
+      <WeatherCard weatherData={weatherData} />
+      <section className="cards">
+        <p className="cards__text">
+          Today is {temp} &deg; F / You may want to wear:
+        </p>
+        <ul className="cards__list">
+          {clothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={onCardClick}
+                />
+              );
+            })}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
