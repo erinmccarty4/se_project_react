37f13382cc25eea9fa73803caf74b962259bd4cd
Your Name <you@example.com>
2024-09-01 17:12:35 -0400

    update

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -19,14 +19,14 @@ function ItemModal({ activeModal, card, onClose, onDelete }) {
             <h2 className="modal__caption">{card.name}</h2>
             <p className="modal__weather">Weather: {card.weather}</p>
           </div>
+          <button
+            type="button"
+            className="modal__delete-button"
+            onClick={handleDeleteCardClick}
+          >
+            Delete item
+          </button>
         </div>
-        <button
-          type="button"
-          className="modal__delete-button"
-          onClick={handleDeleteCardClick}
-        >
-          Delete item
-        </button>
       </div>
     </div>
   );
