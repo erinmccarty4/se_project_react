e020837f518b3d42664e9bd9515b4a399cf810dd
Your Name <you@example.com>
2024-09-01 17:03:07 -0400

    deleteItem

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -15,8 +15,10 @@ function ItemModal({ activeModal, card, onClose, onDelete }) {
         ></button>
         <img src={card.imageUrl} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+          <div>
+            <h2 className="modal__caption">{card.name}</h2>
+            <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
         </div>
         <button
           type="button"
